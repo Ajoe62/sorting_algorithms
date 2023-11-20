3972753fbabc1b482466970167db9d74757f2521
Ajoe62 <ijeborjoe@gmail.com>
2023-11-20 07:02:55 -0800

    shell_sort.c modified

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -24,23 +24,23 @@ void swap_ints(int *a, int *b)
  */
 void shell_sort(int *array, size_t size)
 {
-	size_t gap, i, j;
+	size_t val, i, j;
 
 	if (array == NULL || size < 2)
 		return;
 
-	for (gap = 1; gap < (size / 3);)
-		gap = gap * 3 + 1;
+	for (val = 1; val < (size / 3);)
+		val = val * 3 + 1;
 
-	for (; gap >= 1; gap /= 3)
+	for (; val >= 1; val /= 3)
 	{
-		for (i = gap; i < size; i++)
+		for (i = val; i < size; i++)
 		{
 			j = i;
-			while (j >= gap && array[j - gap] > array[j])
+			while (j >= val && array[j - val] > array[j])
 			{
-				swap_ints(array + j, array + (j - gap));
-				j -= gap;
+				swap_ints(array + j, array + (j - val));
+				j -= val;
 			}
 		}
 		print_array(array, size);
